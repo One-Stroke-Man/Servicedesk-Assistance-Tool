3c1a3908745c19c24733faabdd96580582513815
One-Stroke-Man <dean_morgenrood@live.nl>
2021-06-16 14:48:26 +0200

    init

--- Servicedesk Assistance Tool.ps1
+++ Servicedesk Assistance Tool.ps1
@@ -15,57 +15,57 @@ $form.AutoSizeMode = "GrowOnly"
 $form.BackColor = "White"
 $form.Icon = $Icon
 
-#Button Attribute Changer
+#Button AD Tools
 $Button = New-Object System.Windows.Forms.Button
 $Button.Location = New-Object System.Drawing.Point(35,30)
 $Button.Size = New-Object System.Drawing.Size(150,46)
-$Button.Text = 'Attribute Changer'
+$Button.Text = 'AD Tools'
 $Button.DialogResult = [System.Windows.Forms.DialogResult]::OK
 $form.AcceptButton = $Button
 $form.Controls.Add($Button)
 
-#Button Unlock Account
+#Button Networking Tools
 $Button1 = New-Object System.Windows.Forms.Button
 $Button1.Location = New-Object System.Drawing.Point(35,75)
 $Button1.Size = New-Object System.Drawing.Size(150,46)
-$Button1.Text = 'Unlock AD Account'
+$Button1.Text = 'Networking Tools'
 $Button1.DialogResult = [System.Windows.Forms.DialogResult]::OK
 $form.AcceptButton = $Button1
 $form.Controls.Add($Button1)
 
-#Button Enable Account
+#TBD
 $Button2 = New-Object System.Windows.Forms.Button
 $Button2.Location = New-Object System.Drawing.Point(35,120)
 $Button2.Size = New-Object System.Drawing.Size(150,46)
-$Button2.Text = 'Enable AD Account'
+$Button2.Text = ''
 $Button2.DialogResult = [System.Windows.Forms.DialogResult]::OK
 $form.AcceptButton = $Button2
 $form.Controls.Add($Button2)
 
-#Button Enable Account
+#TBD
 $Button3 = New-Object System.Windows.Forms.Button
 $Button3.Location = New-Object System.Drawing.Point(35,165)
 $Button3.Size = New-Object System.Drawing.Size(150,46)
-$Button3.Text = 'Disable AD Account'
+$Button3.Text = ''
 $Button3.DialogResult = [System.Windows.Forms.DialogResult]::OK
 $form.AcceptButton = $Button3
 $form.Controls.Add($Button3)
 
 #Button Section
 $Button.Add_Click({
-     Invoke-expression (invoke-webrequest -uri "https://raw.githubusercontent.com/One-Stroke-Man/Servicedesk-Assistance-Tool/main/AD/User/AD%20-%20Attribute%20Changer.ps1").Content
+     Invoke-expression (invoke-webrequest -uri "https://raw.githubusercontent.com/One-Stroke-Man/Servicedesk-Assistance-Tool/main/AD/AD%20Tools.ps1").Content
 })
 
 $Button1.Add_Click({
-     Invoke-expression (invoke-webrequest -uri "https://raw.githubusercontent.com/One-Stroke-Man/Servicedesk-Assistance-Tool/main/AD/User/AD%20-%20Unlock%20AD%20User.ps1").Content
+     Invoke-expression (invoke-webrequest -uri "https://raw.githubusercontent.com/One-Stroke-Man/Servicedesk-Assistance-Tool/main/Networking/Networking%20Tools.ps1").Content
 })
 
 $Button2.Add_Click({
-     Invoke-expression (invoke-webrequest -uri "https://raw.githubusercontent.com/One-Stroke-Man/Servicedesk-Assistance-Tool/main/AD/User/AD%20-%20Enable%20AD%20User.ps1").Content
+     Invoke-expression (invoke-webrequest -uri "").Content
 })
 
 $Button3.Add_Click({
-     Invoke-expression (invoke-webrequest -uri "https://raw.githubusercontent.com/One-Stroke-Man/Servicedesk-Assistance-Tool/main/AD/User/AD%20-%20Disable%20AD%20User.ps1").Content
+     Invoke-expression (invoke-webrequest -uri "").Content
 })
 
 $form.ShowDialog()
